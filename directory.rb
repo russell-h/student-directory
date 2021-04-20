0920da616cfcb374e865930b1a840907075f4838
Russell Horth <russell.horth@gmail.com>
2021-04-20 21:26:14 +0100

    directory with extra info, a default name value, name begins with selection and center aligned

--- directory.rb
+++ directory.rb
@@ -5,32 +5,45 @@ def input_students
   students = []
   #get the first name
   name = gets.chomp
+
+  #add a default value if no input given
+  if name.empty? 
+    name = 'default value'
+  end
+
   #while the name is not empty, repeat this code
   while !name.empty? do 
+    puts "What is your crime"
+    my_crime = gets.chomp
+    puts "What is your experience, high or low?" 
+    exp_level = gets.chomp
     #add the student hash to the array
-    students << {name:name, cohort:  :november}
+    students << {name: name, cohort: :november, crime: my_crime, experience_level: exp_level }
+    
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp
   end
   #return the array of students
   students
-
 end
-
-
-
-
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "Only display name if starts with letter.."
+  letter = gets.chomp 
+  students.each_with_index do |student, index|
+  #only list names beginning with first letter....
+    if student[:name].chars.first == letter
+      puts " #{index}: #{student[:name]} (#{student[:cohort]} cohort) and my crime is #{student[:crime]} at a #{student[:experience_level]} level of experience".center(100)
+    end
   end
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
